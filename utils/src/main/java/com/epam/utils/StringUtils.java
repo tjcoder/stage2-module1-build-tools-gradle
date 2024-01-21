447a05c0f0af7e9bdb5eee59a2543d2469b45991
Taalay Joldoshev <gmoftj@gmail.com>
2024-01-21 18:46:20 +0600

    checks if a str is creatable

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         return NumberUtils.isDigits(str) && NumberUtils.toDouble(str) > 0;
+        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
     }
 }
