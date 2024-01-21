1dacfaa44d0cef3d96e4526359494ec64ba119a1
Taalay Joldoshev <gmoftj@gmail.com>
2024-01-21 18:06:49 +0600

    checks if a str is digit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         return NumberUtils.toDouble(str) > 0;
+         return NumberUtils.isDigits(str) && NumberUtils.toDouble(str) > 0;
     }
 }
